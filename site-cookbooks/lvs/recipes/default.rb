4a8d2c3e56fbfe6e4252da2ea9259cf7aed4bb0d
Yuji Kamiya <y.kamiya0@gmail.com>
2014-11-27 22:01:41 +0900

    make recipe of lvs

--- site-cookbooks/lvs/recipes/default.rb
+++ site-cookbooks/lvs/recipes/default.rb
@@ -6,3 +6,36 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+file "/etc/sysctl.conf" do
+    owner "root"
+    group "root"
+    mode "0644"
+    action :create
+end
+
+template "/tmp/iptables.rules" do
+    owner "root"
+    group "root"
+    mode "0644"
+    source "iptables.rules.erb"
+    action :create
+end
+
+bash "restart_iptables" do
+    user "root"
+    code <<-EOC
+        iptables-restore < /tmp/iptables.rules
+    EOC
+end
+
+package "keepalived" do
+    action :install
+end
+
+template "/etc/keepalived/keepalived.conf" do
+    owner "root"
+    group "root"
+    mode "0644"
+    source "keepalived.conf.erb"
+    action :create
+end
